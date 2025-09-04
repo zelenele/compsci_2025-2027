5489a098ab792c9161f796c4a88b015a1b7a062d
Zibilyonik <onatsurmeli@gmail.com>
2025-09-04 10:03:51 +0200

    Add print calls to check updated types

--- 01.Introductions/data_types.py
+++ 01.Introductions/data_types.py
@@ -6,12 +6,20 @@ number = 7
 decimal = 3.5
 word = "hello"
 is_sunny = True
+print(type(number))
+print(type(decimal))
+print(type(word))
+print(type(is_sunny))
 
-# Can you change these values to the type shown in the comment?
+# Change the values to the type shown in the comment?
 age = "15"      # Change to int
 score = 10      # Change to str
 rain = "False"  # Change to bool
 height = "1.75" # Change to float
+print(type(age))
+print(type(score))
+print(type(rain))
+print(type(height))
 
 # What will be printed?
 print(type(2 + 2))
@@ -19,9 +27,3 @@ print(type("5" + "5"))
 print(type(3.0 + 1))
 print(type(True))
 # Write your guesses as comments, then run and check
-
-# Make the statements True by filling in the correct type.
-assert isinstance("school", str) == True
-assert isinstance(42, ____ ) == True
-assert isinstance(2.5, ____ ) == True
-assert isinstance(False, ____ ) == True
