a76a4132122b9b7cb6392e7852823b92612c1798
zelenele <rzepka124@gmail.com>
2025-09-09 16:00:19 +0200

    Update data_types.py

--- 01.Introductions/data_types.py
+++ 01.Introductions/data_types.py
@@ -7,15 +7,19 @@ decimal = 3.5
 word = "hello"
 is_sunny = True
 print(type(number))
+# int
 print(type(decimal))
+#float
 print(type(word))
+# str
 print(type(is_sunny))
+#bool
 
 # Change the values to the type shown in the comment?
-age = "15"      # Change to int
-score = 10      # Change to str
-rain = "False"  # Change to bool
-height = "1.75" # Change to float
+age = 15      # Change to int
+score = "10"      # Change to str
+rain = False  # Change to bool
+height = 1.75 # Change to float
 print(type(age))
 print(type(score))
 print(type(rain))
