836e80e8b7c55fef8456d50d9ba2bf5b77dbdbcd
zelenele <rzepka124@gmail.com>
2025-09-23 15:40:14 +0200

    IDK

---
+++ 02.Operators and String Methods/Italia.py
@@ -0,0 +1,7 @@
+def Pasta_best(Jedznie):
+    
+    if Jedznie == "Pasta":
+        return "Go to italy!"
+    else:
+        return "Nothing is better!"
+Pasta_best(input("What is the best food in the world? "))
--- 02.Operators and String Methods/Store__.py
+++ 02.Operators and String Methods/Store__.py
@@ -1,18 +1,27 @@
-def membership_discount(New_price):
-    membership_discount = final_price * 0.2
-    return membership_discount
+   
 
-def Membership_card(Membership_card):
-    if Membership_card == True:
-         return True
-    else:
-         return False
+def membership_discount(price):
+     membership_discount_value= price * 0.2
+     return membership_discount_value
 
-def final_price(final_price):
-    if Membership_card == True:
-         final_price = final_price - membership_discount
-         return final_price
-    else:
-         final_price = final_price
-         return final_price
-print(final_price(90))
+def Membership_card(Membershipp_card):
+     if Membershipp566565_card == True:
+          return True
+     else:
+          return False
+
+def final_price(New_price, Membershipp_card):
+     if Membershipp_card == True:
+          New_price = price - membership_discount(price)
+          return New_price
+     else:
+          
+          return final_price
+
+   
+     
+price = float(input("Enter the price of your shopping: "))
+Membershipp_card = Membership_card(bool(int(input("Do you have a membership card? (1 for yes, 0 for no): "))))
+membership_discount = membership_discount(price)
+final_price = final_price(price, Membershipp_card)
+print("Your final price is: ", final_price())
